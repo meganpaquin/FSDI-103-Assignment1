44a5ad42ef411d3dc1b12fd6a0f512b3789602af
Megan Paquin <megan.j.paquin@gmail.com>
2022-06-21 21:59:46 -0400

    Create the Variables

---
+++ java.js
@@ -0,0 +1,36 @@
+//list the variables for Gmail//
+
+//the variables for the sign in page//
+let email="megan.j.paquin@gmail.com";
+let phone=8023043654;
+let password="mynewpassword1";
+
+//the variables for a new account creation//
+first_name="megan";
+last_name="paquin";
+let username=first_name+last_name+"@gmail.com";
+const   greeting="Welcome!"
+let password_confirm;
+
+//the variables for the inbox//
+let search_words="what are you looking for?";
+let search_from="who are you looking for?";
+let search_to="who did you send to?";
+let subject="what was the subject?";
+let date_sent=3/19/1994;
+
+//the variables for a new email//
+let to="samantha@gmail.com";
+let subject_newmail="Hello I am a Programmer";
+
+//show off some variables on screen//
+console.log("You need to make a new account");
+document.write(`
+    <h2>${greeting}<br>Thank you for creating a new account <span>${first_name} ${last_name}</span></h2>
+`)
+document.write(`
+    <p>Your username is: <span>${username}</span></p>
+    <p>Your password is: <span>${password}</span></p>
+`)
+console.log("New password Created")
+console.log(`Hello ${first_name} ${last_name}`)
